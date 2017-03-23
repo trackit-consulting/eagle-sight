5f18232ff34736bdf767e0c600126606be17a39a
rubend151 <rubendd@live.com.pt>
2017-03-23 16:50:17 +0000

    Inserting Route Information Layout

--- app/view/Layout/Map/Map.js
+++ app/view/Layout/Map/Map.js
@@ -8,6 +8,96 @@ Ext.define('ES.view.Layout.Map.Map',{
     layout: 'fit',
     controller: 'map',
     viewModel: 'map',
-    center: new google.maps.LatLng(40.350054, -8.5809265)
+    center: new google.maps.LatLng(40.350054, -8.5809265),
+    mapOptions: {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        minZoom: 7,
+        streetViewControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_TOP
+    },
+        zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_CENTER
+    },
+        styles: [{
+            
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [{
+                    "color": "#E4E3E2"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [{
+                    "color": "#0C7CA5"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#E4E3E2"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#FEFEFE"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                        "color": "#FEFEFE"
+                    },
+                    {
+                        "weight": 0.5
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#FEFEFE"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                        "color": "#FEFEFE"
+                    },
+                    {
+                        "weight": 0.5
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#FEFEFE"
+                }]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [{
+                    "color": "#FEFEFE"
+                }]
+            }
+        ]
+    },
+     dockedItems: [{
 
+        xtype: 'mainlist',
+        preventHeader: true,
+        //overlay : true,
+        dock: 'bottom'
+    }]
+    
 });
--- app/view/Layout/Map/MapModel.js
+++ app/view/Layout/Map/MapModel.js
@@ -11,17 +11,23 @@ Ext.define('ES.view.Layout.Map.MapModel', {
                 {lat: 38.6509715, lng: -9.0598949}
             ]
         },
+        Distance: {
+            storeId: 'Distance',
+            autoLoad: true,
+            fields: ['lat', 'lng', 'dist'],
+            data: []
+        },
+        ArrivalTime: {
+            storeId: 'ArrivalTime',
+            autoLoad: true,
+            fields: ['lat', 'lng', 'time'],
+            data: []
+        },
         Polyline: {
             storeId: 'Polyline',
             autoLoad: true,
             fields: ['lat', 'lng'],
-            data: [
-                /*
-                {lat: 38.6619788, lng: -9.0758742},
-                {lat: 38.6449281, lng: -9.0637415},
-                {lat: 38.6509715, lng: -9.0598949}
-                */
-            ]
+            data: []
         }
     }
 });
